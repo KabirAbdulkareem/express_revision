4a1c594301bfe9417371d905fde820c5a4a3647c
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2023-01-29 15:35:28 +0100

    Added Error handling

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require("cors")
+const cors = require("cors");
 const { logger } = require("./middlewares/logEvents");
+const errorHandler = require("./middlewares/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 //custom mddleware logger
 app.use(logger);
 
 //Cross Origin Resource Sharing
-app.use(cors())
+const whitelist = [
+  "http://example1.com",
+  "https://mui-social-dashboard.vercel.app",
+  "http://127.0.0.1:5500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //built-in middleware to handle urlencoded data
 //in othr words, form data:
@@ -34,8 +50,20 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+//error middleware handler for undefiend origin
+//an example of this is when the request is comming from the development server
+//in which case the value of the origin in the header is assigned the value "Undefine"
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
